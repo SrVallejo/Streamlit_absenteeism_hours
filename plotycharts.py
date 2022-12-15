7c652b34ac761336e234eaadec7114ff2ce381b7
newetty <ettyguerra@gmail.com>
2022-12-15 14:15:44 +0100

    plotychart- borrador

---
+++ plotycharts.py
@@ -0,0 +1,50 @@
+
+#------mainpage???---------
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.figure_factory as ff
+
+import plotly_express as px
+
+def stast(dataframe):
+    st.header('Data Statistic')
+    st.writer(dataframe.describe())
+
+def data_header (dataframe):
+    st.header('Data Header')
+    st.write('df.head'())
+
+def plot(dataframe)
+    fig, ax= plt.subplot(1,1)
+    ax.scatter(x=df['Depth'], y= df['Magnitud']
+    ax.set_xlabel('Depth'))
+    ax.set_ylabel('Magnitud')
+    st.pyplot(fig)
+def interactive_plot(dataframe)
+    x_axis_val=st.selectbox('Select X-Axis Value'),
+    options = df.columns
+    y_axis_val=st.selectbox('Select Y-Axis Value'),
+    options = df.columns
+    plot.scatter(dataframe,  x=x_axis_val, y=y_axis_val)
+    col=st.color_picker('Select a graph color')
+    plot.update_traces(marker=dict(color=col)
+    st.plotly_chart(plot)
+
+options = st.radio ('Pages', options =
+'[Data statistic',
+'Data Header', 
+'Plot','' Interactive Plot'])
+
+if upload_file:
+    df= pd.read_csv(upload_file)
+if options=='Data Statistics':
+    (stats(df))
+elif options=='Data Header':
+    data_header(df)
+    elif options=='Plot'
+plot(df)
+    elif options=='Interactive Plot'
+plot(df)    
