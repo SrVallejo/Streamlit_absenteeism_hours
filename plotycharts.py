17c98c20cfa7d0f9fe82d35a862a131d9981899a
newetty <ettyguerra@gmail.com>
2022-12-15 22:37:05 +0100

    graficos borrador

--- plotycharts.py
+++ plotycharts.py
@@ -17,61 +17,36 @@ def data_header (dataframe):
     st.header('Data Header')
     st.write('df.head'())
 
-def plot(dataframe)
+def plot(dataframe):
     fig, ax= plt.subplot(1,1)
-    ax.scatter(x=df['Depth'], y= df['Magnitud']
-    ax.set_xlabel('Depth'))
+    ax.scatter (x=df['Depth'], y= df['Magnitud'])
+    ax.set_xlabel('Depth')
     ax.set_ylabel('Magnitud')
     st.pyplot(fig)
-def interactive_plot(dataframe)
+def interactive_plot(Absenteeism_at_work):
     x_axis_val=st.selectbox('Select X-Axis Value'),
     options = df.columns
     y_axis_val=st.selectbox('Select Y-Axis Value'),
     options = df.columns
     plot.scatter(dataframe,  x=x_axis_val, y=y_axis_val)
     col=st.color_picker('Select a graph color')
-    plot.update_traces(marker=dict(color=col)
+    plot.update_traces(marker=dict(color=col))
     st.plotly_chart(plot)
 
-options = st.radio ('Pages', options =
-'[Data statistic',
-'Data Header', 
-'Plot','' Interactive Plot'])
+options = st.radio ('Pages', options = 
+                    Data Statistics,
+                    Data Header, Plot, Interactive Plot)
 
 if upload_file:
-    df= pd.read_csv(upload_file)
+    df= pd.read_csv('Absenteeism_at_work.csv')
 if options=='Data Statistics':
     (stats(df))
 elif options=='Data Header':
     data_header(df)
-    elif options=='Plot'
-plot(df)
-    elif options=='Interactive Plot'
-plot(df)    
+elif options=='Plot':
+    plot(df)
+elif options=='Interactive Plot':
+
+    plot(df)    
 
 
-#-------------------graficos con groupby----------------
-
-# Absence sum by season (Bar chat)
-
-absenteeism_by_season= (
-
-def_selection.groupby(by=['absenteeism_time_in_hours']).sum().[['season']].sort_values(by='season')
-)
-Fig_absence_season = px.bar(
-    absenteeism_by_season,
-    x= 'season',
-    y = 'absenteeism_by_season'
-    orientation=h,
-    title= <b> Absenteeism_by_season </b>,
-    color_discrete_sequence=[#0083B8]* len[absenteeism_by_season],
-    template= "plotly_white",
-)
-# To quit the grades from ploty
-
-Fig_absence_season.update_layout
-        (plot_bgcolors="rgba"(0,0,0,0)",
-        xaxis=dict(showgrid)
-)
-
-st.ploty_chart(Fig_absence_season)
