369bd35a0a0aa061eb4434ab29764872de619759
SrVallejo <l.vallejo.28@gmail.com>
2022-12-16 13:57:07 +0100

    added heatmap filtered by column

--- pages/6_Correlation.py
+++ pages/6_Correlation.py
@@ -1,14 +1,43 @@
 import streamlit as st
-from main_page import data_set as df
+from main_page import data_set
 import seaborn as sns
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 
-# fig = plt.figure(figsize=(10, 4))
-# sns.heatmap(df.corr(), annot = True)
-# st.pyplot(fig)
 
-fig = plt.figure(figsize=(10, 4))
-sns.countplot(x="Seasons", data=df)
+# List of columns to show
 
-st.pyplot(fig)
+columns_list = []
+
+# Function that creates a checkbox in the sidebar to select the columns
+#If true, it adds the *column_name* to *columns_list*, if not, remove that name
+def create_filter_columns(column_name):
+    #columns hided as default
+    hided_columns = [
+        "ID","Month of absence",
+        "Transportation expense","Distance from Residence to Work","Service time",
+        "Hit target", "Work load Average",
+        "Weight","Height","Education",
+        "Social drinker","Social smoker"
+    ]
+
+    #Las que quité en la predicción "ID","Service time","Month of absence","Weight","Height"
+
+    default = column_name not in hided_columns
+    if st.sidebar.checkbox(column_name, value = default):
+        columns_list.append(column_name)
+    else:
+        try:
+            columns_list.remove(column_name)
+        except:
+            pass
+
+
+#Loop to create a checkbox for each column in the dataset
+st.sidebar.write("Show Columns")
+for column in data_set.columns:
+    create_filter_columns(column)
+
+fig, ax = plt.subplots()
+sns.heatmap(data_set[columns_list].corr(), ax=ax, annot=True)
+st.write(fig)
