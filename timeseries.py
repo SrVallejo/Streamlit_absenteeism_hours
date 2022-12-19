3931f5fb25492e42912b4a41de6cb09e93555738
newetty <ettyguerra@gmail.com>
2022-12-19 19:45:44 +0100

    ploty seanbor

--- timeseries.py
+++ timeseries.py
@@ -12,25 +12,12 @@ import cufflinks
 
 sidebar = st.sidebar
 
-analysis_choice = sidebar.radio("Analysis Type", ["Single", "Multiple"])
-st.markdown(f"Analysis Mode: {analysis_choice}")
 
-if analysis_type=="Single":
-   
-   
-
-    trend_level = sidebar.selectbox("Trend Level", ["month_of_absence','day_of_the_week',
+trend_level = sidebar.selectbox("Trend Level", ["month_of_absence','day_of_the_week',
        'seasons'])
-    st.markdown(f"### Currently Selected {trend_level}")
+st.markdown(f"### Currently Selected {trend_level}")
 
-    show_data = sidebar.checkbox("Show Data")
+fig = sns.lineplot(x = 'month_of_absence', y =  'absenteeism_time_in_hours', data = df,estimator=sum)
+warnings.filterwarnings(action='once')
 
-    trend_kwds = {"Daily": "1D", "Weekly": "1W", "Monthly": "1M", "Quarterly": "1Q", "Yearly": "1Y"}
-    trend_data = data.query(f"location=='{location_selector}'").\
-        groupby(pd.Grouper(key="date",
-        freq=trend_kwds[trend_level])).aggregate(new_cases=("new_cases", "sum"),
-        new_deaths = ("new_deaths", "sum"),
-        new_vaccinations = ("new_vaccinations", "sum"),
-        new_tests = ("new_tests", "sum")).reset_index()
-
-    trend_data["date"] = trend_data.date.dt.date
+st.plotly_chart(fig)
