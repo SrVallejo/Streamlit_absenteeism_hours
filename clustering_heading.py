45bbfa085356a957edfb67c4dffe4c8f869b08f8
newetty <ettyguerra@gmail.com>
2023-01-09 14:41:35 +0100

    Imagines clustering Heading- clustering results

---
+++ clustering_heading.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import streamlit.components.v1 as com
+
+
+com.html("""
+<div>
+<style>
+h1.heading{
+   background.color:+ blueviolet;
+   color:lightyellow;
+   border-radius:20px;
+   text-align:center;
+}
+</style>
+<h1.class="heading">
+<h1>
+This is our clustering 3D result
+</h1>
+<p>
+Despite the fact that there are still values ​​of inertia to go down before achieving stability, we consider the starting from 3 clusters one could consider the beginning of stability and, therefore, where would we find three different groups of employees features
+</div>
+""")
