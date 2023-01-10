c6920db4034a23df4b12d834fc8e2996b5ad744b
SrVallejo <l.vallejo.28@gmail.com>
2023-01-10 12:06:51 +0100

    todo

--- main_page.py
+++ main_page.py
@@ -3,9 +3,12 @@ import pandas as pd
 import pandas.io.sql as sqlio
 import psycopg2
 import settings
+from create_database import clean_dataset
 
 st.markdown("# Main page 🎈")
 st.sidebar.markdown("# Main page 🎈")
+
+
 
 def connect():
     conn = psycopg2.connect(database="Absenteeism",
@@ -14,12 +17,18 @@ def connect():
                             host=settings.HOST,
                             port=settings.PORT)
 
-    #conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-
     cur = conn.cursor()
     return cur, conn
 
-cur, conn = connect()
-sql = 'SELECT * FROM "Absenteeism at work"'
-data_set = sqlio.read_sql_query(sql, conn)
-conn = None
+
+try:
+    cur, conn = connect()
+    sql = 'SELECT * FROM "Absenteeism at work"'
+    data_set = sqlio.read_sql_query(sql, conn)
+    conn = None
+
+except:
+    print("Can't connect to BBDD\nReading Dataset from csv")
+    data_set = pd.read_csv()
+    data_set = pd.read_csv("dataset/Absenteeism_at_work.csv",delimiter=";")
+    data_set = clean_dataset(data_set)
