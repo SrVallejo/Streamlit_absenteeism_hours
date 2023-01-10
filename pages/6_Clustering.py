41b6ed78f5f71c2320f3740f67694d6e6aa76f59
SrVallejo <l.vallejo.28@gmail.com>
2023-01-10 12:31:56 +0100

    text without html

--- pages/6_Clustering.py
+++ pages/6_Clustering.py
@@ -6,24 +6,26 @@ import streamlit as st
 import plotly.express as px
 import streamlit.components.v1 as com
 import streamlit as st
-com.html("""
-<div>
-<style>
-h1.heading{
-   background.color:+ blueviolet;
-   color:lightyellow;
-   border-radius:20px;
-   text-align:center;
-}
-</style>
-<h1.class="heading">
-<h1>
-This is our clustering 3D result
-</h1>
-<p>
-Despite the fact that there are still values ​​of inertia to go down before achieving stability, we consider the starting from 3 clusters one could consider the beginning of stability and, therefore, where would we find three different groups of employees features
-</div>
-""")
+# com.html("""
+# <div>
+# <style>
+# h1.heading{
+#    background.color:+ blueviolet;
+#    color:lightyellow;
+#    border-radius:20px;
+#    text-align:center;
+# }
+# </style>
+# <h1.class="heading">
+# <h1>
+
+# </h1>
+# <p>
+# Despite the fact that there are still values ​​of inertia to go down before achieving stability, we consider the starting from 3 clusters one could consider the beginning of stability and, therefore, where would we find three different groups of employees features
+# </div>
+# """)
+st.header("This is our clustering 3D result")
+st.write("Despite the fact that there are still values ​​of inertia to go down before achieving stability, we consider the starting from 3 clusters one could consider the beginning of stability and, therefore, where would we find three different groups of employees features")
 path_to_html = "./Imagenes/test.html" 
 
 # Read file and keep in variable
